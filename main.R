79a49b431ce4caab5be12db18639cefb819fef4a
christophervcerrachio <christophervcerrachio@gmail.com>
2022-05-18 18:45:30 -0400

    User can now select root children categories from dataframe

--- main.R
+++ main.R
@@ -19,24 +19,68 @@ while(
     cat("Not a valid section, select again\n");
     userInput <- readLines("stdin", n=1);
   }
+cat("\n"); #input/output spacing
 
 #getting/formatting all child categories of root category (use variables in get request later)
 rootChildrenResponse <- GET("https://api.stlouisfed.org/fred/category/children?category_id=0&api_key=03fc1426e63a56cf48dece52f36227ac&file_type=json");
-print(rootChildrenResponse);
 apiResponseData <- fromJSON(rawToChar(rootChildrenResponse$content));
 
-#extract children category id,name and present to user for further selection
-print(typeof(apiResponseData));
-print(apiResponseData);
+
+#extract children category columns id,name for further selection from user
+#make dataframe for id,name from list
+responseDF <- as.data.frame(apiResponseData);
+#isolating id,name columns in new dataframe
+responseDF <- responseDF[, c("categories.id", "categories.name")];
+#changing column names in new dataframe
+colnames(responseDF) <- c("ID", "NAME");
+print(responseDF, row.names=FALSE);
+
 
 #getting/validating id input from user
+categoryIDVector <- responseDF$ID;
+cat("\nPlease select (type) a category id from the table\n");
+userInput <- readLines("stdin", n=1);
+
+userIDFlag <- FALSE;
+while(userIDFlag == FALSE){
+  for(id in categoryIDVector){
+    if(id == userInput){
+      userIDFlag <- TRUE;
+      break;
+    }
+  }
+  if(userIDFlag == FALSE){
+    cat("Not a valid category id, select again\n");
+    userInput <- readLines("stdin", n=1);
+  }
+}
+cat("\n"); #input/output spacing
+
+
+#id selected by user from the dataframe
+selectedID <- userInput;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #get/visualize series from category chosen by user
-
-
-
-
-
-
 
 
 # #default section id = 0 if not specified
