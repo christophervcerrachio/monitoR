fe44ca88521b5d8b1fffa22a98c251cac1d13728
christophervcerrachio <christophervcerrachio@gmail.com>
2022-05-20 14:02:07 -0400

    Modularized API call functions in Fetcher.R, preparing to create main loop design

---
+++ Fetcher.R
@@ -0,0 +1,33 @@
+library(httr);
+library(jsonlite);
+
+#to be run once at selection of category section
+fetcherRoot <- function(){
+  #getting/formatting all child categories of root category (use variables in get request later)
+  rootChildrenResponse <- GET("https://api.stlouisfed.org/fred/category/children?category_id=0&api_key=03fc1426e63a56cf48dece52f36227ac&file_type=json");
+  apiResponseData <- fromJSON(rawToChar(rootChildrenResponse$content));
+  #extract children category columns id,name for further selection from user
+  #make dataframe for id,name from list
+  responseDF <- as.data.frame(apiResponseData);
+  #isolating id,name columns in new dataframe
+  responseDF <- responseDF[, c("categories.id", "categories.name")];
+  #changing column names in new dataframe
+  colnames(responseDF) <- c("ID", "NAME");
+  responseDF;
+}
+
+#x = category id
+#NEED LOGIC/HELPER FUNCTION hasChildren
+fetcherChildren <- function(x){
+  #category id selected by user from the dataframe
+  selectedID <- x;
+  #get children of selected category (need to be REPEATABLE from here, down to situation where category has no children)
+  selectedChildrenConstruct <- c("https://api.stlouisfed.org/fred/category/children?category_id=", selectedID,"&api_key=03fc1426e63a56cf48dece52f36227ac&file_type=json");
+  selectedChildrenConstruct <- paste(selectedChildrenConstruct, collapse="");
+  selectedChildrenConstruct <- GET(selectedChildrenConstruct);
+  apiResponseData <- fromJSON(rawToChar(selectedChildrenConstruct$content));
+  responseDF <- as.data.frame(apiResponseData);
+  responseDF <- responseDF[, c("categories.id", "categories.name")];
+  colnames(responseDF) <- c("ID", "NAME");
+  responseDF;
+}
--- main.R
+++ main.R
@@ -1,5 +1,12 @@
+#test whether dependencies are included by Fether.R
 library(httr);
 library(jsonlite);
+
+#bringing in Fetcher.R (have access to fetcherRoot and fetcherChildren functions)
+source("Fetcher.R");
+
+
+
 
 
 #STARTUP PROMPT######################################
@@ -19,18 +26,15 @@ while(
 cat("\n"); #input/output spacing
 
 
+
+
+
 #CATEGORY INITIAL SHOW######################################
-#getting/formatting all child categories of root category (use variables in get request later)
-rootChildrenResponse <- GET("https://api.stlouisfed.org/fred/category/children?category_id=0&api_key=03fc1426e63a56cf48dece52f36227ac&file_type=json");
-apiResponseData <- fromJSON(rawToChar(rootChildrenResponse$content));
-#extract children category columns id,name for further selection from user
-#make dataframe for id,name from list
-responseDF <- as.data.frame(apiResponseData);
-#isolating id,name columns in new dataframe
-responseDF <- responseDF[, c("categories.id", "categories.name")];
-#changing column names in new dataframe
-colnames(responseDF) <- c("ID", "NAME");
+responseDF <- fetcherRoot();
 print(responseDF, row.names=FALSE);
+
+
+
 
 
 #CATEGORY CHILDREN SHOW######################################
@@ -53,43 +57,10 @@ while(userIDFlag == FALSE){
   }
 }
 cat("\n"); #input/output spacing
-#category id selected by user from the dataframe
-selectedID <- userInput;
-#get children of selected category (need to be REPEATABLE from here, down to situation where category has no children)
-selectedChildrenConstruct <- c("https://api.stlouisfed.org/fred/category/children?category_id=", selectedID,"&api_key=03fc1426e63a56cf48dece52f36227ac&file_type=json");
-selectedChildrenConstruct <- paste(selectedChildrenConstruct, collapse="");
-selectedChildrenConstruct <- GET(selectedChildrenConstruct);
-apiResponseData <- fromJSON(rawToChar(selectedChildrenConstruct$content));
-responseDF <- as.data.frame(apiResponseData);
-responseDF <- responseDF[, c("categories.id", "categories.name")];
-colnames(responseDF) <- c("ID", "NAME");
+
+#fetcherChildren body was HERE
+responseDF <- fetcherChildren(userInput);
 print(responseDF, row.names=FALSE);
-
-
-#CATEGORY CHILDREN SHOW TWO######################################
-#getting/validating id input from user (round 3)
-categoryIDVector <- responseDF$ID;
-cat("\nPlease select (type) a category id from the table for related categories\n");
-userInput <- readLines("stdin", n=1);
-
-userIDFlag <- FALSE;
-while(userIDFlag == FALSE){
-  for(id in categoryIDVector){
-    if(id == userInput){
-      userIDFlag <- TRUE;
-      break;
-    }
-  }
-  if(userIDFlag == FALSE){
-    cat("Not a valid category id, select again\n");
-    userInput <- readLines("stdin", n=1);
-  }
-}
-cat("\n"); #input/output spacing
-
-
-
-
 
 
 
